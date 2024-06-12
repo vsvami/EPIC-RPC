c9387f077154898d4380140d0e75e5c2f746bb22
Vladimir Dmitriev <svami.vd@gmail.com>
2024-06-13 02:35:33 +0400

    Initial Commit

---
+++ EPIC RPC/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EPIC RPC
+//
+//  Created by Vladimir Dmitriev on 13.06.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
