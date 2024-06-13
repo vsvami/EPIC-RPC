f25b0c62bbc7d7d21dc46c5101cb9fdd6bc81b64
Dmitry <mrsahadov@gmail.com>
2024-06-13 16:13:59 -0300

    Add Rule struct

---
+++ EPIC RPC/Views/Rule.swift
@@ -0,0 +1,14 @@
+//
+//  Rule.swift
+//  EPIC RPC
+//
+//  Created by Дмитрий Волков on 13.06.2024.
+//
+
+import Foundation
+
+struct Rule {
+    let text: String
+    let image: String
+    let subpoint: Bool
+}
